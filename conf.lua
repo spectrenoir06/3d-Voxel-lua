59f92de472f413513a5021a40085b76a4c1f0ad0
Spectre <antoine.doussaud.org>
2020-05-07 18:24:24 +0200

    big opti + anim

--- conf.lua
+++ conf.lua
@@ -7,7 +7,7 @@ function love.conf(t)
 	t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean)
 	t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
-	t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
+	-- t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
 
 	t.window.title = ""-- The window title (string)
 	t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
@@ -26,7 +26,7 @@ function love.conf(t)
 	t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
 	t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
 
-	t.modules.audio = true              -- Enable the audio module (boolean)
+	-- t.modules.audio = true              -- Enable the audio module (boolean)
 	t.modules.data = true               -- Enable the data module (boolean)
 	t.modules.event = true              -- Enable the event module (boolean)
 	t.modules.font = true               -- Enable the font module (boolean)
--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 vector = require "vector"
-
+local ffi = require "ffi"
 
 function drawVerticalLine(x, y, y2, color)
-	if y <= y2 then
+	if y2>0 and y <= y2 then
+		-- print(x,y, y2, y2-y)
 		love.graphics.setColor(color)
-		love.graphics.line(x, y, x, y2)
+		love.graphics.rectangle("fill", x, y, 1, y2-y)
+		-- love.graphics.line(x, y, x, y2)
 	end
 end
 
@@ -12,7 +14,12 @@ function pack(...)
 	return { n = select("#", ...), ... }
 end
 
+local colormap = {}
+local heightmap_1D = {}
+local heightmap_2D = {}
 
+local timer = 0
+local frame = 0
 
 function love.load(arg)
 	love.graphics.setLineStyle("rough")
@@ -21,29 +28,32 @@ function love.load(arg)
 	colormap_data  = love.image.newImageData("C1W.png")
 	map =  love.graphics.newImage(colormap_data)
 
-	-- canvas = love.graphics.newCanvas( 800, 600 )
-
-	heightmap = {}
-
 	for x=0, heightmap_data:getWidth()-1 do
-		heightmap[x+1] = {}
+		-- heightmap[x+1] = {}
 		for y=0,heightmap_data:getHeight()-1 do
 			-- print(x,y)
-			heightmap[x+1][y+1] = heightmap_data:getPixel(x, y)*255
+			heightmap_1D[x+1+y*heightmap_data:getHeight()] = heightmap_data:getPixel(x, y)*255
 		end
 	end
 
-	colormap = {}
-
-	for x=0, colormap_data:getWidth()-1 do
-		colormap[x+1] = {}
-		for y=0,colormap_data:getHeight()-1 do
+	for x=0, heightmap_data:getWidth()-1 do
+		heightmap_2D[x+1] = {}
+		for y=0,heightmap_data:getHeight()-1 do
 			-- print(x,y)
-			colormap[x+1][y+1] = pack(colormap_data:getPixel(x, y))
+			heightmap_2D[x+1][y+1] = heightmap_data:getPixel(x, y)*255
 		end
 	end
 
-	canvas = love.graphics.newCanvas(320, 240)
+
+	-- for x=0, colormap_data:getWidth()-1 do
+	-- 	colormap[x+1] = {}
+	-- 	for y=0,colormap_data:getHeight()-1 do
+	-- 		-- print(x,y)
+	-- 		colormap[x+1][y+1] = pack(colormap_data:getPixel(x, y))
+	-- 	end
+	-- end
+
+	canvas = love.graphics.newCanvas(320*2, 240*2)
 	canvas:setFilter("nearest", "nearest")
 
 	pos = vector(512, 512)
@@ -58,11 +68,16 @@ end
 function render(p, phi, height, horizon, scale_height, distance, screen_width, screen_height)
 
 	local floor = math.floor
-	local sinphi = math.sin(phi);
-	local cosphi = math.cos(phi);
+	local sinphi = math.sin(phi)
+	local cosphi = math.cos(phi)
+	local x = p.x
+	local y = p.y
+
+	local rect = love.graphics.rectangle
+	local color = love.graphics.setColor
 
 	-- initialize visibility array. Y position for each column on screen
-	local ybuffer = {}
+	local ybuffer = ffi.new("float[?]", screen_width+1)
 	for i=1,screen_width do
 		-- print("buff:",i)
 		ybuffer[i] = screen_height
@@ -71,45 +86,39 @@ function render(p, phi, height, horizon, scale_height, distance, screen_width, s
 	local dz = 1.0
 	local z = 1.0
 
+
 	-- Draw from back to the front (high z coordinate to low z coordinate)
 	while z < distance do
 		-- print(z)
-
-	-- for z=1,distance do
-	-- for z=distance,1,-1 do
 		-- Find line on map. This calculation corresponds to a field of view of 90°
+		local pleft_x = -cosphi*z - sinphi*z + x
+		local pleft_y =  sinphi*z - cosphi*z + y
 
-		pleft =
-		{
-			(-cosphi*z - sinphi*z) + p.x,
-			( sinphi*z - cosphi*z) + p.y
-		}
-
-		pright =
-		{
-			( cosphi*z - sinphi*z) + p.x,
-			(-sinphi*z - cosphi*z) + p.y
-		}
+		local pright_x =  cosphi*z - sinphi*z + p.x
+		local pright_y = -sinphi*z - cosphi*z + p.y
 
 		-- segment the line
-		local dx = (pright[1] - pleft[1]) / screen_width
-		local dy = (pright[2] - pleft[2]) / screen_width
+		local dx = (pright_x - pleft_x) / screen_width
+		local dy = (pright_y - pleft_y) / screen_width
 
 		-- Raster line and draw a vertical line for each segment
 		for i=0, screen_width-1 do
-			local x = floor(pleft[1]%1023)+1
-			local y = floor(pleft[2]%1023)+1
+			local ybuff = screen_height
+			local x = floor(pleft_x)%1024
+			local y = floor(pleft_y)%1024
 			-- print(x,y,i,ybuffer[i+1])
-			local height_on_screen = floor((height - heightmap[x][y]) / z * scale_height + horizon)
-			-- print(z,i,height_on_screen)
-			-- drawVerticalLine(i, height_on_screen, ybuffer[i+1], {z/200,0.01,0.01})
-			drawVerticalLine(i+1, height_on_screen, ybuffer[i+1], colormap[x][y])
-			-- drawVerticalLine(i, height_on_screen, 600, colormap[x][y])
-			if height_on_screen < ybuffer[i+1] then
+			local height_on_screen = floor((height - heightmap_2D[x+1][y+1]) / z * scale_height + horizon)
+
+			local y2 = ybuffer[i+1]
+			-- print(y2)
+			if y2>0 and height_on_screen<y2 then
+				color(colormap_data:getPixel(x, y+(1024*frame)))
+				rect("fill", i, height_on_screen, 1, y2-height_on_screen)
 				ybuffer[i+1] = height_on_screen
 			end
-			pleft[1] = pleft[1] + dx
-			pleft[2] = pleft[2] + dy
+
+			pleft_x = pleft_x + dx
+			pleft_y = pleft_y + dy
 		end
 		z = z + dz
 		if z > 200 then
@@ -121,17 +130,21 @@ end
 function love.draw()
 	canvas:renderTo(function()
 		love.graphics.clear(56/255, 108/255, 193/255)
-		render(pos, dir:toPolar().x, height, vx, vy, dist, 320, 240)
-	end);
+		render(pos, dir:toPolar().x, height, vx, vy, dist, 320*2, 240*2)
+	end)
 	love.graphics.setColor(1, 1, 1, 1)
-	love.graphics.draw(canvas,0,0,0,2,2);
+	love.graphics.draw(canvas,0,0,0,1,1);
 	love.graphics.draw(map,0,240*2,0,0.625,0.625)
 	love.graphics.circle("fill", (pos.x%1024)*0.625, (pos.y%1024)*0.625 + 240*2, 5, segments)
 	love.graphics.print(love.timer.getFPS(), 200, 5)
 end
 
-
 function love.update(dt)
+	timer = timer + dt
+	if timer > 0.250 then
+		frame = (frame + 1)%4
+		timer = 0
+	end
 	if love.keyboard.isDown("space") then height = height + (dt * 100) end
 	if love.keyboard.isDown("lshift") then height = height - (dt * 100) end
 
@@ -159,8 +172,7 @@ function love.update(dt)
 	if love.keyboard.isDown("h") then vy = vy + 1 end
 	if love.keyboard.isDown("j") then vy = vy - 1 end
 
-
-	local sol = heightmap[math.floor(pos.x%1023)+1][math.floor(pos.y%1023)+1]
+	local sol = heightmap_2D[math.floor(pos.x%1023)+1][math.floor(pos.y%1023)+1]
 
 	if height < sol + 10 then
 		height = sol + 10
